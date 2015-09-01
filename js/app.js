94a998a2a204fe781be954a5f63858381cdb7bae
Vitaliy Ribachenko <vit@ribachenko.com>
2015-09-01 21:36:57 +0300

    Fixed mobile layout, added JS for checking cover height on mobile

--- js/app.js
+++ js/app.js
@@ -8,13 +8,34 @@
     new ImageFullscreen({
       selector: '.post-content img'
     });
+
+    onResize();
   };
 
+  /**
+   * Listener for onResize event.
+   */
+  var onResize = function () {
+    var coverContainer = document.querySelector('.cover-container');
+    
+    if (coverContainer && coverContainer.style.backgroundImage) {
+      var containerIsSmall = coverContainer.getBoundingClientRect().height <= window.innerHeight;
+      document.body.style.height = containerIsSmall ? '100%' : 'auto';
+    }
+  };
+
+
+
+  /**
+   * Attach events
+   */
 
   if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', onReady);
   } else {
     onReady();
   }
+
+  window.addEventListener('resize', onResize);
 
 })();

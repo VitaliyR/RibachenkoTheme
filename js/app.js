1377ca077390b80b7f87189ca594c220ee8c9463
Vitaliy Ribachenko <vit@ribachenko.com>
2015-09-04 21:22:56 +0300

    Workstage at implementing auto overflow for cover articles

--- js/app.js
+++ js/app.js
@@ -26,9 +26,26 @@
    * it's height to match 100%
    */
   var onResize = function () {
-    if (coverContainer && coverContainer.style.backgroundImage) {
+    if (coverContainer && coverContainer.style.backgroundImage) { // if we are on cover
+      // check cover container height to be either 100% of body (if not too much content or auto
       var containerIsSmall = parseInt(coverContainer.getBoundingClientRect().height, 10) <= window.innerHeight;
       document.body.style.height = containerIsSmall ? '100%' : 'auto';
+
+      // check for main.content with articles - if its content fits the design column
+      var articlesContainerHeight = parseInt(
+        window.getComputedStyle(
+          document.querySelector('.blog-stories-wrapper')
+        ).height,
+        10);
+      var articlesHeader = document.querySelector('.blog-stories-header').getBoundingClientRect().height;
+      var articles = document.querySelector('main');
+      var firstArticle = articles.children[0];
+
+      if (firstArticle) {
+        var firstArticleStyle = window.getComputedStyle(firstArticle);
+        var articlesHeight = articlesContainerHeight - articlesHeader;
+        articles.style.maxHeight = articlesHeight + 'px';
+      }
     }
   };
 
